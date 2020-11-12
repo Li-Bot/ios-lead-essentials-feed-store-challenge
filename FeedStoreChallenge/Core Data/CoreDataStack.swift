05c2edbe0cd4cc30d99f9aaf61b300d5ecf2a153
Libor Polehna <polehna.libor@gmail.com>
2020-11-12 14:58:06 +0400

    Removed required constructor in CoreDataStack protocol.

--- FeedStoreChallenge/Core Data/CoreDataStack.swift
+++ FeedStoreChallenge/Core Data/CoreDataStack.swift
@@ -6,8 +6,6 @@ import CoreData
 public protocol CoreDataStack {
     
     var managedContext: NSManagedObjectContext { get }
-    
-    init(storeURL: URL)
 
     func deleteAll(of entityName: String, context: NSManagedObjectContext) -> Error?
     func saveContext(context: NSManagedObjectContext) -> Error?
